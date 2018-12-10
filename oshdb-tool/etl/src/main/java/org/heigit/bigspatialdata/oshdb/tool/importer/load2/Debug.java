8b5f69c9392c2cee9ef75bd2fe9c40b71a10829a
Rafael Troilo <rafael.troilo@uni-heidelberg.de>
2018-12-10 17:53:59 +0100

    .

---
+++ oshdb-tool/etl/src/main/java/org/heigit/bigspatialdata/oshdb/tool/importer/load2/Debug.java
@@ -0,0 +1,9 @@
+package org.heigit.bigspatialdata.oshdb.tool.importer.load2;
+
+public class Debug {
+	
+	public static final long node = -1;
+	public static final long way = -1;
+	public static final long rel = -1;
+
+}
