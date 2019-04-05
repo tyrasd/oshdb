759a1e3d8b5907b2110f2c2cc3705fbc264e488c
Rafael Troilo <rafael.troilo@uni-heidelberg.de>
2019-04-05 15:24:39 +0200

    .

--- oshdb-tool/etl/src/main/java/ImportH2.java
+++ oshdb-tool/etl/src/main/java/ImportH2.java
@@ -1,9 +1,13 @@
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.RandomAccessFile;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
 import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -30,6 +34,9 @@ import org.heigit.bigspatialdata.oshdb.grid.GridOSHWays;
 import org.heigit.bigspatialdata.oshdb.index.XYGrid;
 import org.heigit.bigspatialdata.oshdb.index.zfc.ZGrid;
 import org.heigit.bigspatialdata.oshdb.tool.importer.cli.validator.DirExistValidator;
+import org.heigit.bigspatialdata.oshdb.tool.importer.extract.Extract.KeyValuePointer;
+import org.heigit.bigspatialdata.oshdb.tool.importer.extract.data.Role;
+import org.heigit.bigspatialdata.oshdb.tool.importer.extract.data.VF;
 import org.heigit.bigspatialdata.oshdb.util.CellId;
 import org.heigit.bigspatialdata.oshdb.util.OSHDBBoundingBox;
 import com.beust.jcommander.JCommander;
@@ -51,6 +58,11 @@ public class ImportH2 {
         required = true, order = 10)
     public Path workDir;
 
+    @Parameter(names = {"-extractDir"},
+        description = "extract directory", validateWith = DirExistValidator.class,
+        required = true, order = 10)
+    public Path extractDir;
+    
     @Parameter(names = {"--out"}, description = "output path", required = true)
     public Path output;
 
@@ -74,12 +86,14 @@ public class ImportH2 {
     }
 
     final Path workDir = config.workDir;
+    final Path extractDir = config.extractDir;
     final Path outDir = config.output;
     
     final Path oshdb = config.oshdb;
 
     Class.forName("org.h2.Driver");
-    try (Connection conn = DriverManager.getConnection("jdbc:h2:" + oshdb.toString() + "", "sa", "")) {
+    try (Connection conn = DriverManager.getConnection("jdbc:h2:" + oshdb.toString() + "", "sa", "");
+        Statement stmt = conn.createStatement()) {
         
         stream("node", workDir, conn, outDir,
             (xyId, zoom, index, data) -> new GridOSHNodes(xyId, zoom, 0L, 0L, 0L, 0L, index, data));
@@ -87,6 +101,18 @@ public class ImportH2 {
             (xyId, zoom, index, data) -> new GridOSHWays(xyId, zoom, 0L, 0L, 0L, 0L, index, data));
         stream("relation", workDir,conn , outDir, (xyId, zoom, index,
             data) -> new GridOSHRelations(xyId, zoom, 0L, 0L, 0L, 0L, index, data));
+        
+        stmt.executeUpdate(
+            "drop table if exists metadata ; create table if not exists metadata (key varchar primary key, value varchar)");
+        loadMeta(conn, extractDir.resolve("extract_meta"));
+        stmt.executeUpdate(
+            "drop table if exists key ; create table if not exists key (id int primary key, txt varchar)");
+        stmt.executeUpdate(
+            "drop table if exists keyvalue; create table if not exists keyvalue (keyId int, valueId int, txt varchar, primary key (keyId,valueId))");
+        loadTags(conn, extractDir.resolve("extract_keys"), extractDir.resolve("extract_keyvalues"));
+        stmt.executeUpdate(
+            "drop table if exists role ; create table if not exists role (id int primary key, txt varchar)");
+        loadRoles(conn, extractDir.resolve("extract_roles"));
 
     }
   }
@@ -366,6 +392,136 @@ public class ImportH2 {
       }
     }
   }
+  
+  public static void loadMeta(Connection conn, Path meta) {
+    try (
+        PreparedStatement pstmt =
+            conn.prepareStatement("insert into metadata (key,value) values(?,?)");
+        BufferedReader br = new BufferedReader(new FileReader(meta.toFile()));) {
+
+
+      String line = null;
+      while ((line = br.readLine()) != null) {
+        if (line.trim().isEmpty())
+          continue;
+
+        String[] split = line.split("=", 2);
+        if (split.length != 2)
+          throw new RuntimeException("metadata file is corrupt");
+
+        pstmt.setString(1, split[0]);
+        pstmt.setString(2, split[1]);
+        pstmt.addBatch();
+      }
+
+
+      pstmt.setString(1, "attribution.short");
+      pstmt.setString(2, "© OpenStreetMap contributors");
+      pstmt.addBatch();
+
+      pstmt.setString(1, "attribution.url");
+      pstmt.setString(2, "https://ohsome.org/copyrights");
+      pstmt.addBatch();
+
+      pstmt.setString(1, "oshdb.maxzoom");
+      pstmt.setString(2, "" + 14);
+      pstmt.addBatch();
+
+      pstmt.setString(1, "oshdb.flags");
+      pstmt.setString(2, "expand");
+      pstmt.addBatch();
+
+      pstmt.executeBatch();
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void loadTags(Connection conn, Path keys, Path keyValues) {
+    try (
+        final DataInputStream keyIn =
+            new DataInputStream(MoreFiles.asByteSource(keys).openBufferedStream());
+        final RandomAccessFile raf = new RandomAccessFile(keyValues.toFile(), "r");
+        final FileChannel valuesChannel = raf.getChannel();) {
+
+      final int length = keyIn.readInt();
+      int batch = 0;
+      try {
+
+        PreparedStatement insertKey =
+            conn.prepareStatement("insert into key (id,txt) values (?,?)");
+        PreparedStatement insertValue =
+            conn.prepareStatement("insert into keyvalue ( keyId, valueId, txt ) values(?,?,?)");
+
+        for (int keyId = 0; keyId < length; keyId++) {
+          final KeyValuePointer kvp = KeyValuePointer.read(keyIn);
+          final String key = kvp.key;
+
+          insertKey.setInt(1, keyId);
+          insertKey.setString(2, key);
+          insertKey.executeUpdate();
+          valuesChannel.position(kvp.valuesOffset);
+
+          DataInputStream valueStream = new DataInputStream(Channels.newInputStream(valuesChannel));
+
+          long chunkSize = (long) Math.ceil((double) (kvp.valuesNumber / 10.0));
+          int valueId = 0;
+          for (int i = 0; i < 10; i++) {
+            long chunkEnd = valueId + Math.min(kvp.valuesNumber - valueId, chunkSize);
+            for (; valueId < chunkEnd; valueId++) {
+              final VF vf = VF.read(valueStream);
+              final String value = vf.value;
+
+              insertValue.setInt(1, keyId);
+              insertValue.setInt(2, valueId);
+              insertValue.setString(3, value);
+              insertValue.addBatch();
+              batch++;
+
+              if (batch >= 100_000) {
+                insertValue.executeBatch();
+                batch = 0;
+              }
+            }
+          }
+        }
+        insertValue.executeBatch();
+      } catch (SQLException e) {
+        throw new IOException(e);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void loadRoles(Connection conn, Path roles) {
+    try (DataInputStream roleIn =
+        new DataInputStream(MoreFiles.asByteSource(roles).openBufferedStream())) {
+      try {
+
+        PreparedStatement insertRole =
+            conn.prepareStatement("insert into role (id,txt) values(?,?)");
+        for (int roleId = 0; roleIn.available() > 0; roleId++) {
+          final Role role = Role.read(roleIn);
+
+          insertRole.setInt(1, roleId);
+          insertRole.setString(2, role.role);
+          insertRole.executeUpdate();
+          System.out.printf("load role:%6d(%s)%n", roleId, role.role);
+
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    } catch (IOException e1) {
+      e1.printStackTrace();
+    }
+  }
+  
+  
 
   public static String hRBC(long bytes) {
     final int unit = 1024;
